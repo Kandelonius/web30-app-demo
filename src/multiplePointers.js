133c38ff84466ee8621c0568397cea1f23cfea55
Shane Kaestner <shane.kaestner@yahoo.com>
2020-09-13 14:24:43 -0700

    completed multiple pointers section.

--- src/multiplePointers.js
+++ src/multiplePointers.js
@@ -37,3 +37,57 @@ function sumZero(arr) {
 console.log("the first combination found is " + sumZero([-4, -3, -2, -1, 0, 1, 2, 5]))
 console.log("the first combination found is " + sumZero([-4, -3, -2, -1, 0, 5, 6, 8]))
  */
+
+ /*
+ countUniqueValues which accepts a sorted array and counts the unique values in the array, negative numbers are allowed.
+
+function countUniqueValues(arr) {
+    let obj = {};
+    let count = 0;
+    // console.log(arr[0]);
+    for(let i = 0; i < arr.length; i++){
+        // console.log("hi");
+        // console.log(arr[i]);
+        if(!(obj[arr[i]])){
+            obj[arr[i]] = 1;
+            count++;
+        }
+    }
+    return count;
+    // return obj.length;
+};
+
+console.log(countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]))
+
+instructor solution
+function countUniqueValues(arr){
+    if(arr.length === 0) return 0;
+    var i = 0;
+    for(var j = 1; j < arr.length; j++){
+        if(arr[i] !== arr[j]){
+            i++;
+            arr[i] = arr[j]
+        }
+    }
+    return i + 1;
+}
+countUniqueValues([1,2,2,5,7,7,99])
+*/
+function countUniqueValues(arr) {
+    let index = 0;
+    // let value = 0;
+    for(let i = 0; i < arr.length; i++){
+        if(i === 0) {
+            arr[index] = index + 1;
+            index++;
+            // value = arr[i];
+        }else if(arr[i] !== arr[index]){
+            arr[index] = index + 1;
+            index++;
+            // value = arr[i];
+        }
+    }
+    return arr[index];
+};
+
+console.log(countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]))
