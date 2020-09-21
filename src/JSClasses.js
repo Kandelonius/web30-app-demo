aabdacfcdf05a1f347a51cda463c70a96883c3d3
Shane Kaestner <shane.kaestner@yahoo.com>
2020-09-21 15:11:37 -0700

    update Data structures lecture preparing to work on linked list exercise.

---
+++ src/JSClasses.js
@@ -0,0 +1,54 @@
+class Student {
+    constructor(firstName, lastName, year) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.year = year;
+        this.tardies = 0;
+        this.scores = [];
+    }
+    fullName() { // instance method
+        return `Your full name is ${this.firstName} ${this.lastName}`;
+    }
+    markLate() { // instance method
+        this.tardies++;
+        if(this.tardies >= 3) {
+            return "YOU ARE EXPELLED!!!";
+        }
+        return `${this.firstName} ${this.lastName} has been late ${this.tardies} times`
+    }
+    addScores(score) { // instance method
+        this.scores.push(score);
+        return this.scores;
+    }
+    calculateAverage() { // instance method
+        let sum = this.scores.reduce(function(a, b){return a + b});
+        return sum / this.scores.length;
+    }
+    static EnrollStudents() {
+        return "Enrolling Students";
+    }
+}
+
+let firstStudent = new Student("Derek", "Zoolander");
+let secondStudent = new Student("Blue", "Steele");
+
+console.log(firstStudent.fullName());
+
+for(let i = 0; i < 2; i++) {
+    secondStudent.markLate();
+}
+
+
+console.log(secondStudent.tardies);
+console.log(secondStudent.markLate());
+console.log(firstStudent.scores);
+
+firstStudent.addScores(57);
+firstStudent.addScores(92);
+firstStudent.addScores(97);
+
+console.log(firstStudent.scores);
+
+console.log(firstStudent.calculateAverage());
+
+console.log(Student.EnrollStudents());
---
+++ src/addTwoNumbersLL.js
@@ -0,0 +1,12 @@
+/**
+ * using a singly linked list that will add two numbers where the digits are stored in reverse order
+ * and output the sum in another linked list also in reverse order.
+ * 342 + 465 = 807 would look like:
+ * (2 -> 4 -> 3) + (5 -> 6 -> 4) = 7 -> 0 -> 8
+ */
+
+var addTwoNumbers = function(l1, l2) {
+    // first take the input unmbers and load them into a SLL with the first digit as the tail and last digit as the head.
+    
+    
+};
