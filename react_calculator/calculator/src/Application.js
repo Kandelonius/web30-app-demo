72174305e231e662f7a0f72ca50760656af80307
Shane Kaestner <shane.kaestner@yahoo.com>
2020-11-12 10:47:36 -0800

    worked on button functionallity.

--- react_calculator/calculator/src/Application.js
+++ react_calculator/calculator/src/Application.js
@@ -1,13 +1,52 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import HighScore from './HighScore'
 
 class Application extends Component {
+    constructor(props) {
+        super(props);
 
-    render(){
+        this.state = {
+            count: 0,
+            overTen: false
+        }
+    }
+
+    handleClick = () => {
+        this.setState({ count: this.state.count + 1 });
+    }
+
+    componentDidUpdate(props, state) { // track state
+        if (this.state.count > 10 && this.state.count !== state.count && !this.state.overTen) {
+            this.setState({ overTen: true });
+        }
+        // console.log("Updated from", state, "to", this.state);
+    }
+
+    resetCount = (e) => {
+        this.setState({
+            count: 0,
+            overTen: false
+        })
+    }
+
+    render() {
+        let { count } = this.state;
+
 
         let name = "Shane";
 
 
-        return <h1>Hello {name}</h1>;
+        return (<div>
+            <h1>Hello {name} You clicked the button {count} times</h1>
+            <HighScore
+                overTen={this.state.overTen}
+                onReset={this.resetCount}
+            />
+
+            <span>
+                <button onClick={(e) => this.handleClick()}>Click Me</button>
+            </span>
+        </div>)
     }
 
 }
