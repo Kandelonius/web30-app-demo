d861e45cb14b69fcfe626c6f5fae49eeb0831491
Shane Kaestner <shane.kaestner@yahoo.com>
2020-11-12 18:19:17 -0800

    I am now able to at least print out the value of each button into the console. No logic yet.

--- react_calculator/calculator/src/calculatorComponents/Calculator.jsx
+++ react_calculator/calculator/src/calculatorComponents/Calculator.jsx
@@ -4,10 +4,45 @@ import Keys from './Keys';
 
 function Calculator() {
 
+    const [value, setValue] = useState(0);
+
+    const handleOperation = (value) => {
+        console.log(value);
+    }
+
     return (<div>
-        
-            <Display />
-            <Keys />
+
+        <Display />
+        <div id="keyBorder">
+            <div id="firstRow">
+                <Keys keyValue={7} onClick={handleOperation} />
+                <Keys keyValue={8} onClick={handleOperation} />
+                <Keys keyValue={9} onClick={handleOperation} />
+                <Keys keyValue={"*"} onClick={handleOperation} />
+            </div>
+            <div id="secondRow">
+                <Keys keyValue={4} onClick={handleOperation} />
+                <Keys keyValue={5} onClick={handleOperation} />
+                <Keys keyValue={6} onClick={handleOperation} />
+                <Keys keyValue={"/"} onClick={handleOperation} />
+
+            </div>
+            <div id="thirdRow">
+                <Keys keyValue={1} onClick={handleOperation} />
+                <Keys keyValue={2} onClick={handleOperation} />
+                <Keys keyValue={3} onClick={handleOperation} />
+                <Keys keyValue={"-"} onClick={handleOperation} />
+            </div>
+            <div id="fourthRow">
+                <Keys keyValue={0} onClick={handleOperation} />
+                <Keys keyValue={"+"} onClick={handleOperation} />
+                <Keys keyValue={"."} onClick={handleOperation} />
+            </div>
+            <div>
+                <Keys keyValue={"C"} onClick={handleOperation} />
+                <Keys keyValue={"="} onClick={handleOperation} />
+            </div>
+        </div>
     </div>
     )
 }
--- react_calculator/calculator/src/calculatorComponents/Keys.jsx
+++ react_calculator/calculator/src/calculatorComponents/Keys.jsx
@@ -1,35 +1,14 @@
 import React, { useEffect, useState } from 'react';
 
-function Keys() {
+function Keys(props) {
 
     const [value, setValue] = useState(0);
 
     // const handleClick = () => {
     //     this.setState({ count: this.state.count + 1 });
     // }
-
-    return (<div>
-        <div id="firstRow">
-            <span><button onClick={() => setValue(7)}>7</button></span>
-            <span><button onClick={() => setValue(8)}>8</button></span>
-            <span><button onClick={() => setValue(9)}>9</button></span>
-            {/* <span><button onClick={() => setValue(*)}>*</button></span> */}
-        </div>
-        <div id="secondRow">
-            <span><button onClick={() => setValue(4)}>4</button></span>
-            <span><button onClick={() => setValue(5)}>5</button></span>
-            <span><button onClick={() => setValue(6)}>6</button></span>
-        </div>
-        <div id="thirdRow">
-            <span><button onClick={() => setValue(1)}>1</button></span>
-            <span><button onClick={() => setValue(2)}>2</button></span>
-            <span><button onClick={() => setValue(3)}>3</button></span>
-        </div>
-        <div id="fourthRow">
-            <span><button onClick={() => setValue(0)}>0</button></span>
-        </div>
-    </div>
-    )
+    return (<button onClick={() => props.onClick(props.keyValue)}>{props.keyValue}</button>
+    );
 }
 
 export default Keys;
