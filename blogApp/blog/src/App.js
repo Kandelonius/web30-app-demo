598f8da6080e85e838b137bd3cdc0d95a5d291f4
Shane Kaestner <shane.kaestner@yahoo.com>
2020-11-19 00:46:01 -0800

    implemented early routing using RRD.

--- blogApp/blog/src/App.js
+++ blogApp/blog/src/App.js
@@ -1,13 +1,19 @@
 import Wrapper from './components/Wrapper';
 import Home from './components/pages/Home';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Wrapper>
-        <Home />
-      </Wrapper>
-      
+      <Router>
+        <Wrapper>
+          <Route
+            path="/"
+            component={Home}
+          />
+        </Wrapper>
+      </Router>
+
     </div>
   );
 }
