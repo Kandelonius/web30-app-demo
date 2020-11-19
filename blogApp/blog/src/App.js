c8631c9e02532333d2f3e76f029bf9b90d1910a0
Shane Kaestner <shane.kaestner@yahoo.com>
2020-11-19 10:09:49 -0800

    routed the about page still with theme elements. Also added a Header component and am working on that.

--- blogApp/blog/src/App.js
+++ blogApp/blog/src/App.js
@@ -1,6 +1,7 @@
 import Wrapper from './components/Wrapper';
 import Home from './components/pages/Home';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import About from './components/pages/About';
 
 function App() {
   return (
@@ -8,12 +9,16 @@ function App() {
       <Router>
         <Wrapper>
           <Route
+            exact={true}
             path="/"
             component={Home}
           />
+          <Route
+            path="/about"
+            component={About}
+          />
         </Wrapper>
       </Router>
-
     </div>
   );
 }
